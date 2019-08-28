93849270329897b732591100756c91885f9a3978
Morgan J <Moggelicious@hotmail.com>
2019-08-28 13:33:13 +0200

    this commit will
    add functions to axios data Class

--- src/Components/Service/Data.ts
+++ src/Components/Service/Data.ts
@@ -3,7 +3,35 @@ import axios from 'axios';
 
 class Data {
 
-    function readBookings
+    readData() {
+        axios.get('http://localhost/react/malina-backend-php/api/booking/read.php')
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+    }
+
+    createData(createData: any) {
+        axios.post('http://localhost/react/malina-backend-php/api/booking/create.php', createData, {
+          headers: { 'Content-Type': 'text/plain' 
+        }}).then((response: any) => {
+          console.log(response);
+          return response;
+        }).catch((error: any) => {
+          console.log(error);
+        });
+      }
+
+    deleteData() {
+
+    }
+
+    updateData() {
+
+    }
 
 }
 
