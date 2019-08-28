abd2033b97c52ad5aa7fa5a3fdf7a4d350c9d852
Morgan J <Moggelicious@hotmail.com>
2019-08-28 13:53:09 +0200

    this commit will clean up Service Data class

--- src/Components/Service/Data.ts
+++ src/Components/Service/Data.ts
@@ -1,38 +1,49 @@
-
 import axios from 'axios';
 
 class Data {
 
     readData() {
         axios.get('http://localhost/react/malina-backend-php/api/booking/read.php')
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-
+            .then(response => {
+                console.log(response.data);
+                return response;
+            }).catch(error => {
+                console.log(error);
+            });
     }
 
     createData(createData: any) {
         axios.post('http://localhost/react/malina-backend-php/api/booking/create.php', createData, {
-          headers: { 'Content-Type': 'text/plain' 
-        }}).then((response: any) => {
-          console.log(response);
-          return response;
-        }).catch((error: any) => {
-          console.log(error);
-        });
-      }
-
-    deleteData() {
-
+            headers: { 'Content-Type': 'text/plain' }})
+            .then((response: any) => {
+                console.log(response);
+                return response;
+            }).catch((error: any) => {
+                console.log(error);
+            });
     }
 
-    updateData() {
-
+      deleteData(deleteData: any) {
+        axios.post('http://localhost/react/malina-backend-php/api/booking/delete.php', deleteData, {
+            headers: { 'Content-Type': 'text/plain' }})
+            .then((response: any) => {
+                console.log(response);
+                return response;
+            }).catch((error: any) => {
+                console.log(error);
+            });
     }
 
-}
+    updateData(updateData: any) {
+        axios.post('http://localhost/react/malina-backend-php/api/booking/update.php', updateData, {
+            headers: { 'Content-Type': 'text/plain' }})
+            .then((response: any) => {
+                console.log(response);
+                return response;
+            }).catch((error: any) => {
+                console.log(error);
+            });
+        }
+    }
 
 export default Data;
