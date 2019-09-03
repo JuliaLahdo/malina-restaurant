c7cf2e0e56ea5dee18134528f548c6b2a309198d
Julia Lahdo <julia.lahdo@live.se>
2019-09-03 11:05:31 +0200

    Add menu as object in separate file

---
+++ src/Service/MenuData.ts
@@ -0,0 +1,73 @@
+class MenuData {
+    menu = {
+        starters: {
+            topic: [
+                "Chanterelle Toast",
+                "Burratina Mozzarella",
+                "Dublin Bay Prawn soup"
+            ],
+            content: [
+                "Bleak roe and classic Västerbotten cheese",
+                "Pine nuts, browned butter & asparagus pepper",
+                "Celeriac, chili and a piece of lobster"
+            ],
+            price: [
+                "115",
+                "95",
+                "145"
+            ]
+        },
+        mains: {
+            topic: [
+                "Penne Pellicci",
+                "Tartar Fillet of Flank",
+                "Grilled Entrecôte",
+                "Classic Cheeseburger",
+                "Deep-fried Goat Cheese",
+                "Risotto with Langoustine",
+                "Burrata Salad"
+            ],
+            content: [
+                "Homemade pesto, tomato sauce, pine nuts, fresh baby spinach, parmigiano and olive oil",
+                "Bleak roe, horseradish emulsion, potato and dill",
+                "Tomato salad, béarnaise sauce and chips",
+                "Lettuce, onion, tomato and our secret dressing",
+                "Beetroots, fresh beans, suger snaps and kale",
+                "Mussels, taleggio, pistachio, trout roe and rosemary oil",
+                "Radish, pickled onion, apple, tomato and pecorino"
+            ],
+            price: [
+                "185",
+                "275",
+                "245",
+                "195",
+                "195",
+                "235",
+                "205"
+            ]
+        },
+        desserts: {
+            topic: [
+                "Wild Strawberry Tartlet",
+                "Coupe Colonel",
+                "Chocolate Truffle",
+                "Cheese Tray"
+            ],
+            content: [
+                "Honey, olive oil and thyme",
+                "Lemon sorbet with vodka",
+                "Sprinkled with cocoa",
+                "4 different cheeses, fig marmalade, red grapes"
+            ],
+            price: [
+                "105",
+                "95",
+                "45",
+                "145"
+            ]
+        }
+        
+    }
+}
+
+export default MenuData;
