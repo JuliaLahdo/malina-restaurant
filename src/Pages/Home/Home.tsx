84f545b36625c770e742701f1e02396553ce262d
Julia Lahdo <julia.lahdo@live.se>
2019-09-08 20:02:58 +0200

    Make CSS and add content for home-component

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 import {Link} from 'react-router-dom';
 import relax from '../../Images/indexoutside.jpg';
+import table from '../../Images/indextable.png';
 import './Home.css';
 
 class Home extends React.Component {
     render() {
         return (
             <div>
+            <Header images="headerImages" title="Welcome to Malina">
+            </Header>
                 <div className="homeContainer">
-                    <Header images="headerImages" title="Welcome to Malina">
-                        <Link to="/Booking">Booking</Link>
-                    </Header>
 
-                    {/* <div className="container"> */}
-                        {/* <img src={relax} alt="Malina restaurant window" className="windowImage" /> */}
-                    {/* </div> */}
+                    <h1 className="pageHeading">Food is art</h1>
+                    <p className="pageDescription">We believe that you eat with your eyes. Not with eyes alone of course, but it's a big part of a nice dish. We combine artistic dishes with an indulgence of flavours with every bite. Welcome to Malina.</p>
+
+                    <Link to="/Booking"><img src={table} alt="Table with white cloth" className="reservationImage"/></Link><br />
+                    <Link to="/Booking" className="reservationsLink">Table reservations</Link>
+
+                    <div className="imageContainer">
+                        <img src={relax} alt="Malina restaurant window" className="windowImage" />
+                    </div>
                 </div>
             </div>
         )
