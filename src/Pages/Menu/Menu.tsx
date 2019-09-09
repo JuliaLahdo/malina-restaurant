e55cef1a9ee0662180cb06f8818ec596dc6c2a57
Julia Lahdo <julia.lahdo@live.se>
2019-09-09 17:57:14 +0200

    Add css for Menu
    Menu items needs centering

--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuData from '../../Service/MenuData';
 import Header from '../../Components/Header/Header';
 import './Menu.css';
+import food from '../../Images/menufoodimage.jpg';
 
 export interface IMenuData {
     type: string;
@@ -22,44 +23,58 @@ class Menu extends React.Component {
         for (let i = 0; i < menu.menu.length; i++) {
             if(menu.menu[i].type === "starter") {
                 startersItemContainer.push((<div key={i}>
-                    <p className="itemTitle">{menu.menu[i].title}</p>
+                    <div className="titlePriceContainer">
+                        <h3 className="itemTitle">{menu.menu[i].title}</h3>
+                        <p className="itemPrice">{menu.menu[i].price}</p>
+                    </div>
                     <p className="itemDescription">{menu.menu[i].description}</p>
-                    <p className="itemPrice">{menu.menu[i].price}</p>
                 </div>));
             }
 
             if(menu.menu[i].type === "main") {
                 mainsItemContainer.push((<div key={i}>
-                    <p className="itemTitle">{menu.menu[i].title}</p>
+                    <div className="titlePriceContainer">
+                        <h3 className="itemTitle">{menu.menu[i].title}</h3>
+                        <p className="itemPrice">{menu.menu[i].price}</p>
+                    </div>
                     <p className="itemDescription">{menu.menu[i].description}</p>
-                    <p className="itemPrice">{menu.menu[i].price}</p>
                 </div>));
             }
 
             if(menu.menu[i].type === "dessert") {
                 dessertsItemContainer.push((<div key={i}>
-                    <p className="itemTitle">{menu.menu[i].title}</p>
+                    <div className="titlePriceContainer">
+                        <h3 className="itemTitle">{menu.menu[i].title}</h3>
+                        <p className="itemPrice">{menu.menu[i].price}</p>
+                    </div>
                     <p className="itemDescription">{menu.menu[i].description}</p>
-                    <p className="itemPrice">{menu.menu[i].price}</p>
                 </div>));
             }
         }
 
-        console.log(menu.menu);
-
         return (
             <div>
                 <Header images="menuImages" title="Our Menu" />
-                        
-                   
-                <h3>Starter</h3>
-                {startersItemContainer}
 
-                <h3>Main</h3>
-                {mainsItemContainer}
+                <div className="pageHeaderContainer">
+                    <h1 className="pageHeading">With a little love</h1>
+                </div>
+                <p className="pageDescription">With a little love we hope to bring joy to our food and other people. We focus on good fresh food, grown as locally and naturally as possible. Enjoy!</p>
 
-                <h3>Dessert</h3>
-                {dessertsItemContainer}
+                <div className="menuContainer">
+                    <h2 className="categoryHeader">Starters</h2>
+                    {startersItemContainer}
+
+                    <h2 className="categoryHeader">Mains</h2>
+                    {mainsItemContainer}
+
+                    <h2 className="categoryHeader">Desserts</h2>
+                    {dessertsItemContainer}
+                </div>
+
+                <div className="imageContainer">
+                    <img src={food} alt="Malina restaurant window" className="windowImage" />
+                </div>
             </div>
         )
     }
