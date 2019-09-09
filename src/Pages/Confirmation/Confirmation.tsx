e5f809bc1f79c6d846860fbb46419467997c77ae
eunjuhuss <eunjuhuss@hotmail.com>
2019-09-09 21:19:37 +0200

    get a customers infomations for confirmation

--- src/Pages/Confirmation/Confirmation.tsx
+++ src/Pages/Confirmation/Confirmation.tsx
@@ -2,25 +2,76 @@ import React, { Component } from 'react';
 import Header from '../../Components/Header/Header';
 import './Confirmation.css';
 import {Link} from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+// import { withRouter } from 'react-router-dom';
+// import Data from '../../Service/Data'
+// import { any } from 'prop-types';
 // import queryString from 'query-string';
 
 
-interface IProps {
+interface IConfirmProps {
     location: any
 }
 
-class Confirmation extends Component<IProps, {}> {
+// interface IBooking {
+//     id: number;
+// }
+
+// interface IState {
+//     latestBooking:  IBooking[];
+// }
+
+class Confirmation extends Component<IConfirmProps, {}> {
+    constructor(props:any) {
+        super(props);
+
+        // this.state = {
+        //  latestBooking: []
+        // };  
+    }
 
     componentDidMount() {
-        console.log(this.props.location.search)
+        // let latestBookingInfo = [];
+        // console.log(this.props.location.search);
+        // const getBookingId = new Data();
+        // getBookingId.readData()
+        // .then(response => {
+        //     console.log(response.data);
+            // for(let i = 0; i < response.data.length; i++){
+            //     if(response.data.booking[i].getBookingId === this.props.location.serch){
+            //         console.log(response.data.booking[i]);
+                    // latestBookingInfo.push(response.data.booking[i]);
+                   
+
+                // }
+            // })
+            
+        // })
+
+        axios.get('http://localhost:8888/api/booking/read.php')
+        .then(response => {
+            console.log(response.data.bookings[0]);
+            //  for(let i = 0; i < response.data.bookings.length; i++){
+            //     if(response.data.bookings[i].id === this.props.location.serch){
+            //         console.log(response.data.booking[i]);
+            //         latestBookingInfo.push(response.data.booking[i]);
+            //     }
+
+            //     }
+            // return response;
+        }).catch(error => {
+            console.log(error);
+        });
+
+
     }
 
     render() {
         return (
             <div>
                    <Header images="menuImages" title="Thanks for booking" />
-                        <Link to="/"><h3>Return to Home</h3></Link>                    
+                        <Link to="/"><h3>Return to Home</h3></Link> 
+                        
             </div>
         )
     }
