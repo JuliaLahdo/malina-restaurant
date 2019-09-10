507e065615fad326898fcd68daff80eb199af1ab
eunjuhuss <eunjuhuss@hotmail.com>
2019-09-10 17:06:18 +0200

    wirte test for navbar and booking pages

--- src/Pages/Booking/Booking.test.tsx
+++ src/Pages/Booking/Booking.test.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Booking from './Booking';
-import { shallow } from 'enzyme';
+import Enzyme,{ shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-describe('First React component test with Enzyme', () => {
-  it('renders without crashing', () => {
-     shallow(<Booking />);
-   });
-});
+
+Enzyme.configure({ adapter: new EnzymeAdapter()});
+
+  it('shoule render without error', () => {
+    const wrapper = shallow(<Booking />);
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('shoule change GDPR state', () => {
+    const wrapper = shallow(<Booking />);
+   expect(wrapper.state("isCheckedGdpr")).toBe(false);
+   // Find a checkbox
+   const toggle = wrapper.find('input[type="checkbox"]');
+   expect(toggle).toHaveLength(1);
+  //onChange
+   toggle.simulate("change");
+   expect(wrapper.state("isCheckedGdpr")).toBe(true);
+  });
+
