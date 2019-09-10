f9b017a0ff6d7ff6e13c6f40e12170d5552c543c
Julia Lahdo <julia.lahdo@live.se>
2019-09-10 21:03:44 +0200

    Add finishing styling for 404

--- src/Pages/NotFound404/NotFound404.tsx
+++ src/Pages/NotFound404/NotFound404.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 import {Link} from 'react-router-dom';
 import './NotFound404.css';
-import { FaTimes } from 'react-icons/fa';
 
 class NotFound404 extends React.Component {
     componentDidMount() {
@@ -13,12 +12,13 @@ class NotFound404 extends React.Component {
         return (
             <div>
             <Header images="errorHeaderImage" title="404"/>
-                <div className="pageHeaderContainer">
-                    <h1 className="pageHeading">Page not found</h1>
-                    <FaTimes className="footerIcon" />
+                <div className="notFoundContainer">
+                    <div className="pageHeaderContainer">
+                        <h1 className="pageHeading">Page not found</h1>
+                    </div>
+                    <h3 className="notFoundText">Unfortunately, the page you are looking for is currently unavaliable</h3>
+                    <Link to="/" className="returnHomeLink"><h4>Return home</h4></Link>
                 </div>
-                <h3 className="notFoundText">Unfortunately, the page you are looking for is currently unavaliable</h3>
-                <Link to="/" className="returnHomeLink"><h4>Return home</h4></Link>
             </div>
         )
     }
