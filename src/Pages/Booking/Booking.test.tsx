0eade6dfa55b4eb3ef2ed404de2febbb43c17352
eunjuhuss <eunjuhuss@hotmail.com>
2019-09-10 15:07:09 +0200

    try to write test

--- src/Pages/Booking/Booking.test.tsx
+++ src/Pages/Booking/Booking.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Booking from './Booking';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Booking />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+     shallow(<Booking />);
+   });
 });
